5c927681636e5b008315fefa6d80b809dfe4fad4
Andrew Cloete <andrewhcloete@gmail.com>
2023-10-09 07:17:11 +0200

    Prep for Corne

--- keyboards/redox/keymaps/andrew/keymap.c
+++ keyboards/redox/keymaps/andrew/keymap.c
@@ -9,18 +9,11 @@
 #define _NAV 2
 #define _ADJUST 3
 
-enum custom_keycodes {
-  QWERTY = SAFE_RANGE,
-  SYMB,
-  NAV,
-  ADJUST,
-  KC_WIN_TAB,
-  MACRO_PTT
-};
+enum custom_keycodes { QWERTY = SAFE_RANGE, SYMB, NAV, ADJUST, KC_WIN_TAB, MACRO_PTT };
 
 // Shortcut to make keymap more readable
-#define SYM_L   MO(_SYMB)
-#define SYM_A   MO(_ADJUST)
+#define SYM_L MO(_SYMB)
+#define SYM_A MO(_ADJUST)
 
 #define KC_ALAS LALT_T(KC_PAST)
 #define KC_CTPL LCTL_T(KC_BSLS)
@@ -31,31 +24,20 @@ enum custom_keycodes {
 
 #define KC_ADEN LT(_ADJUST, KC_END)
 
-
 // Unicode
 
-enum unicode_names {
-    BANG,
-    IRONY,
-    SNEK,
-    WAVE,
-    THUMB_UP,
-    UCE1,
-    UCE2,
-    UCO1,
-    UCO2
-};
+enum unicode_names { BANG, IRONY, SNEK, WAVE, THUMB_UP, UCE1, UCE2, UCO1, UCO2 };
 
 const uint32_t PROGMEM unicode_map[] = {
-    [BANG]  = 0x203D,  // ‽
-    [IRONY] = 0x2E2E,  // ⸮
-    [SNEK]  = 0x1F40D, // 🐍
-    [WAVE]  = 0x1F44B,
-    [THUMB_UP] = 0x1F44D, // 
-    [UCE1] = 0x00EB, // ë
-    [UCE2] = 0x00EA, // ê 
-    [UCO1] = 0x00F6, // ö
-    [UCO2] = 0x00F4 // ô
+    [BANG]     = 0x203D,  // ‽
+    [IRONY]    = 0x2E2E,  // ⸮
+    [SNEK]     = 0x1F40D, // 🐍
+    [WAVE]     = 0x1F44B,
+    [THUMB_UP] = 0x1F44D, //
+    [UCE1]     = 0x00EB,  // ë
+    [UCE2]     = 0x00EA,  // ê
+    [UCO1]     = 0x00F6,  // ö
+    [UCO2]     = 0x00F4   // ô
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -63,11 +45,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case KC_WIN_TAB:
             if (record->event.pressed) {
                 // when keycode KC_WIN_TAB is pressed
-                register_code(KC_LWIN); 
-                register_code(KC_TAB);        
+                register_code(KC_LWIN);
+                register_code(KC_TAB);
             } else {
-                // when keycode KC_WIN_TAB is released         
-                unregister_code(KC_TAB); 
+                // when keycode KC_WIN_TAB is released
+                unregister_code(KC_TAB);
                 unregister_code(KC_LWIN);
             }
             break;
@@ -75,12 +57,11 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // See https://docs.qmk.fm/#/feature_macros?id=send_string-amp-process_record_user
             if (record->event.pressed) {
                 SEND_STRING(SS_LWIN("5") SS_LCTL("d"));
-            } 
+            }
             break;
     }
     return true;
 }
-
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -102,13 +83,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______ ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_F11  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,KC_EXLM ,XXXXXXX,XP(UCE1, UCE2),KC_AMPR,KC_UNDS ,_______ ,                       _______ ,KC_CIRC,KC_1    ,KC_2    ,KC_3    ,KC_DLR ,XXXXXXX ,
+     _______ ,KC_EXLM ,XP(UCO1, UCO2),XP(UCE1, UCE2),KC_AMPR,KC_UNDS ,_______ ,                       _______ ,KC_CIRC,KC_1    ,KC_2    ,KC_3    ,KC_DLR ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     SYM_A   ,KC_AT   ,XXXXXXX,XP(UCO1, UCO2),XXXXXXX,KC_MINS,  KC_LPRN ,                      KC_RPRN ,KC_HASH,KC_4    ,KC_5    ,KC_6    ,KC_ASTR ,KC_GRV  ,
+     SYM_A   ,KC_AT   ,KC_BTN2, KC_MS_U, KC_BTN1, KC_MINS,  KC_LPRN ,                         KC_RPRN ,KC_HASH, KC_4    ,KC_5    ,KC_6    ,KC_ASTR ,KC_GRV  ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______ ,X(THUMB_UP),KC_EXLM,KC_LPRN ,KC_RPRN ,KC_TILD ,_______ ,_______ ,      KC_TILD ,_______ ,KC_PERC ,KC_7    ,KC_8    ,KC_9    ,KC_TILD ,KC_EXLM ,
+     _______ ,XXXXXXX, KC_MS_L,KC_MS_D ,KC_MS_R ,KC_TILD , _______ ,_______ ,        KC_TILD ,_______ ,KC_PERC ,KC_7    ,KC_8    ,KC_9    ,KC_TILD ,KC_EXLM ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX,        KC_0    ,XXXXXXX ,XXXXXXX  ,XXXXXXX 
+     _______ ,_______ ,_______ ,_______ ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    KC_0,         XXXXXXX  ,XXXXXXX ,XXXXXXX  ,XXXXXXX 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -116,11 +97,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,KC_MS_U ,XXXXXXX ,XXXXXXX ,_______ ,                          _______ ,KC_WH_L ,KC_WH_D ,KC_WH_U ,KC_WH_R ,XXXXXXX ,XXXXXXX ,
+     XXXXXXX ,KC_WBAK ,KC_WFWD ,KC_LCBR ,KC_RCBR ,XXXXXXX ,_______ ,                          _______ ,KC_HOME ,KC_PGDN ,KC_PGUP ,XXXXXXX ,KC_END  ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,KC_MS_L ,KC_MS_D ,KC_MS_R ,XXXXXXX ,_______ ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,KC_BTN3 ,XXXXXXX ,
+     XXXXXXX ,KC_LALT, KC_PSCR ,KC_LPRN ,KC_RPRN ,XXXXXXX ,_______ ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,KC_BTN3 ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     SYM_A   ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,_______ ,        KC_PGUP ,KC_PGDN ,KC_BTN1 ,KC_BTN2 ,KC_BTN4 ,KC_BTN5 ,XXXXXXX ,XXXXXXX ,
+     SYM_A   ,XXXXXXX ,KC_DEL  ,KC_LBRC ,KC_RBRC ,XXXXXXX ,_______ ,_______ ,        KC_PGUP ,KC_PGDN ,KC_WH_L ,KC_WH_D ,KC_WH_U ,KC_WH_R ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     _______ ,    _______ ,_______ ,        _______ ,_______ ,    XXXXXXX ,     KC_HOME ,KC_PGDN ,KC_PGUP ,KC_END 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
@@ -136,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,XXXXXXX ,        XXXXXXX ,_______ ,XXXXXXX ,KC_F7   ,KC_F8   ,KC_F9   ,KC_MPLY ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        XXXXXXX ,XXXXXXX ,    KC_F10  ,     KC_F11  ,KC_F12  ,XXXXXXX ,XXXXXXX 
+     XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,     XXXXXXX ,    XXXXXXX ,XXXXXXX ,        KC_F10  ,KC_F11 ,     KC_F12  ,     XXXXXXX, XXXXXXX ,XXXXXXX ,XXXXXXX 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   )
 };
