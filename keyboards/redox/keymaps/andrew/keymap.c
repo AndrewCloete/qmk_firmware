ee48761327de7f8b27003dd7ee87508bbc6e9a5a
Andrew Cloete <andrewhcloete@gmail.com>
2023-10-15 12:32:32 +0200

    Good working Corne

--- keyboards/redox/keymaps/andrew/keymap.c
+++ keyboards/redox/keymaps/andrew/keymap.c
@@ -71,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      LALT_T(KC_TAB),KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    , MACRO_PTT,                   SYM_A  ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,LT(_SYMB, KC_EQL),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_NAESC ,KC_A    ,KC_S    ,KC_D    ,KC_F   ,KC_G    ,KC_LBRC ,                          KC_RBRC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,LT(_NAV, KC_SCLN) ,LALT_T(KC_QUOT),
+     KC_NAESC ,KC_A    ,KC_S    ,KC_D    ,KC_F   ,KC_G    ,KC_LBRC ,                          KC_RBRC ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,LT(_NAV, KC_SCLN) ,KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     SYM_L   ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B   ,C(KC_LWIN),SYM_L ,        KC_HOME ,KC_ADEN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH , KC_BSLS,
+     SYM_L   ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B   ,C(KC_LWIN),SYM_L ,        KC_HOME ,KC_ADEN ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH , LALT_T(KC_BSLS),
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      SYM_A   ,KC_PSCR ,KC_DEL , KC_LALT ,     KC_LCTL ,    KC_LSFT ,KC_LWIN ,        KC_BSPC  ,KC_SPC  ,    KC_ENT ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
@@ -97,9 +97,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,                                            _______ ,_______ ,_______ ,_______ ,_______ ,_______ ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,KC_WBAK ,KC_WFWD ,KC_LCBR ,KC_RCBR ,XXXXXXX ,_______ ,                          _______ ,KC_HOME ,KC_PGDN ,KC_PGUP ,KC_WFAV ,KC_END  ,XXXXXXX ,
+     XXXXXXX ,KC_MS_BTN8,KC_MS_BTN9,KC_LPRN,KC_RPRN,XXXXXXX ,_______ ,                          _______ ,KC_HOME ,KC_PGDN ,KC_PGUP ,KC_WFAV ,KC_END  ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     XXXXXXX ,KC_LALT, KC_PSCR ,KC_LPRN ,KC_RPRN ,KC_UNDS ,_______ ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,KC_BTN3 ,XXXXXXX ,
+     XXXXXXX ,KC_LALT, KC_PSCR ,KC_LCBR ,KC_RCBR ,KC_UNDS ,_______ ,                          _______ ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,KC_BTN3 ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      SYM_A   ,XXXXXXX ,KC_DEL  ,KC_LBRC ,KC_RBRC ,XXXXXXX ,_______ ,_______ ,        KC_PGUP ,KC_PGDN ,KC_WH_L ,KC_WH_D ,KC_WH_U ,KC_WH_R ,XXXXXXX ,XXXXXXX ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
